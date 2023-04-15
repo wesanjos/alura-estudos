3f305701dadb4178762061969b44ae2e07f03da8
Wesley Anjos <wanjos.andrade@gmail.com>
2023-04-15 10:20:18 -0300

    2 - Aula 02 - Realizando criação de workspaces e pacote utils

---
+++ next-arquitetura/projects/web-public/pages/index.tsx
@@ -0,0 +1,10 @@
+import { sum } from "@alura-estudo/utils/math/sum"
+
+export default function HomeScreen() {
+  return (
+    <main>
+      <h1>Home Screen</h1>
+      <p>Importação de módulo local: @alura/utils/math {sum(2, 2)}</p>
+    </main>
+  );
+}
