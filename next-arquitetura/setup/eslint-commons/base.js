bd0a9bc748f4c34aaac2dc2f2c6eeb7990f4aa09
Wesley Anjos <wanjos.andrade@gmail.com>
2023-04-17 22:03:15 -0300

    2 - Aula 03 - Configurando ESlint e Prettier

---
+++ next-arquitetura/setup/eslint-commons/base.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "extends": [
+    "next",
+    "eslint:recommended",
+    "plugin:prettier/recommended"
+  ],
+  "rules": {
+    "@next/next/no-html-link-for-pages": "off"
+  }
+}
