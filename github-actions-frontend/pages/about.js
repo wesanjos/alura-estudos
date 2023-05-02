6b6e2a27ef865e8c23da0d6750516c892b60926d
Wesley Anjos <wesley.anjos@sapientag2.com.br>
2023-05-02 09:40:03 -0300

    Iniciando curso NextJS CI/CD

---
+++ github-actions-frontend/pages/about.js
@@ -0,0 +1,7 @@
+export default function AboutScreen() {
+  return (
+    <div>
+      <h1>GitHub Actions - About</h1>
+    </div>
+  )
+}
---
+++ github-actions-frontend/pages/index.js
@@ -0,0 +1,7 @@
+export default function HomeScreen() {
+  return (
+    <div>
+      <h1>GitHub Actions - Home</h1>
+    </div>
+  )
+}
