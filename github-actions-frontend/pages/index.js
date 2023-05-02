38a203e2a563a5dd7ea26065a217207026af95d6
Wesley Anjos <wesley.anjos@sapientag2.com.br>
2023-05-02 11:50:41 -0300

    Iniciando criação do CI Linter (Testes)

--- github-actions-frontend/pages/index.js
+++ github-actions-frontend/pages/index.js
@@ -1,7 +1,7 @@
 export default function HomeScreen() {
-  return (
-    <div>
-      <h1>GitHub Actions - Home</h1>
-    </div>
-  );
-}
+      return (
+        <div>
+          <h1>GitHub Actions - Home</h1>
+        </div>
+      );
+    }
