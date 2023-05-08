7b1ec2ca41d996921226d4eff3684b85f18d5923
Wesley Anjos <wanjos.andrade@gmail.com>
2023-05-08 20:43:03 -0300

    update

--- github-actions-frontend/pages/index.js
+++ github-actions-frontend/pages/index.js
@@ -1,11 +1,8 @@
-
 export default function HomeScreen() {
   return (
     <div>
       <h1>GitHub Actions - Home</h1>
-      
       Testando conteúdo de CD
-
       <a href="/sobre">Sobre</a>
     </div>
   );
