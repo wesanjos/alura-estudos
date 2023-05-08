ef47c08c29e8aa5346557c6b6f434443ed3afa3f
Wesley Anjos <wanjos.andrade@gmail.com>
2023-05-08 20:36:36 -0300

    update

--- github-actions-frontend/pages/index.js
+++ github-actions-frontend/pages/index.js
@@ -1,7 +1,12 @@
+import NextLink from "next/link";
+
 export default function HomeScreen() {
   return (
     <div>
       <h1>GitHub Actions - Home</h1>
+      <NextLink href="/sobre">
+        <a>Ir para sobre</a>
+      </NextLink>
     </div>
   );
 }
