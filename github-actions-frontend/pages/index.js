4d0127494e56cbece76a1aa1ef65699f2416d60f
Wesley Anjos <wanjos.andrade@gmail.com>
2023-05-08 21:28:20 -0300

    atualizações

--- github-actions-frontend/pages/index.js
+++ github-actions-frontend/pages/index.js
@@ -1,7 +1,7 @@
 export default function HomeScreen() {
   return (
     <div>
-      <h1>GitHub Actions - Home</h1>
+      <h1>GitHub Actions - Home [ TESTANDO ALTERAÇÃO ]</h1>
       Testando conteúdo de CD
       <a href="/sobre">Sobre</a>
     </div>
--- github-actions-frontend/scripts/pullRequestPreview.js
+++ github-actions-frontend/scripts/pullRequestPreview.js
@@ -1,10 +1,13 @@
-import { execSync } from 'child_process';
+import { execSync } from "child_process";
 
-console.log('[DEPLOY_PREVIEW]: Start');
+console.log("[DEPLOY_PREVIEW]: Start");
 
-const command = "yarn deploy:staging"
-const output = execSync(command, { encoding: 'utf8' });
+const command = "yarn deploy:staging";
+const output = execSync(command, { encoding: "utf8" });
 
-console.log('[DEPLOY_PREVIEW]: End');
+const outputLines = output.split("\n");
+const DEPLOY_URL = outputLines[outputLines.length - 1];
 
+console.log("[DEPLOY_PREVIEW]: End");
 
+console.log(`You can see the deploy preview on: ${DEPLOY_URL}`);
