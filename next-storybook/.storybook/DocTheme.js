cd0690d9302a4c084855f18faae59df759907323
Wesley Anjos <wesley.anjos@sapientag2.com.br>
2023-05-24 16:43:23 -0300

    feat: manipulando tema do storybook

---
+++ next-storybook/.storybook/DocTheme.js
@@ -0,0 +1,9 @@
+import { create, darken } from '@storybook/theming'
+
+export default create({
+  base: 'dark',
+  brandTitle: 'Alura - Curso Storybook',
+  brandUrl: 'https://www.wessanjos.dev/',
+  brandImage: 'https://place-hold.it/150x150',
+  brandTarget: '_self'
+})
