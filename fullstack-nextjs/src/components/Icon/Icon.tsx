e24be4f9007c01222731320050c27d5d58c9a835
Wesley Anjos <wesley.anjos@sapientag2.com.br>
2023-07-28 10:20:43 -0300

    feat: criado componente de Icon

---
+++ fullstack-nextjs/src/components/Icon/Icon.tsx
@@ -0,0 +1,44 @@
+import { BaseComponent } from "@src/theme/BaseComponent";
+import * as icons from "./svgs/_index";
+import { StyleSheet } from "@src/theme/StyleSheet";
+
+const iconSizes = {
+  xs: "12px",
+  sm: "16px",
+  md: "24px",
+  lg: "32px",
+  xl: "36px",
+};
+
+interface IconProps {
+  name: keyof typeof icons;
+  styleSheet?: StyleSheet;
+  size?: keyof typeof iconSizes;
+}
+
+export default function Icon({ size, name }: IconProps) {
+  const CurrentIcon = icons[name];
+
+  if (!CurrentIcon) return <>"{name}" is not a valid <Icon /></>;
+
+  return (
+    <BaseComponent
+      as="svg"
+      styleSheet={{
+        width: iconSizes[size],
+        height: iconSizes[size],
+      }}
+      color="inherit"
+      fill="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <CurrentIcon />
+    </BaseComponent>
+  );
+}
+
+Icon.defaultProps = {
+  name: 'default_icon',
+  size: "md",
+};
