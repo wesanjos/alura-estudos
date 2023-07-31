ff4ba0d3106f7a9daf53cb3fa58b9de70d555d6d
Wesley Anjos <wanjos.andrade@gmail.com>
2023-07-31 20:42:21 -0300

    feat: imeplementação de stateless widget e listview

--- first_application/lib/main.dart
+++ first_application/lib/main.dart
@@ -20,37 +20,68 @@ class MyApp extends StatelessWidget {
           appBar: AppBar(
             title: const Text('Tarefas'),
           ),
-          body: Container(
-            child: Stack(
-              children: [
-                Container(
-                  color: Colors.blue,
-                  height: 140,
-                ),
-                Container(
-                  color: Colors.white,
-                  height: 100,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        color: Colors.black26,
-                        width: 72,
-                        height: 100,
-                      ),
-                      const Text('Aprender Flutter'),
-                      ElevatedButton(
-                          onPressed: () {},
-                          child: const Icon(Icons.arrow_drop_up_outlined))
-                    ],
-                  ),
-                )
-              ],
-            ),
+          body: ListView(
+            children: const [
+              Task('Aprender Flutter 1'),
+              Task('Aprender Flutter 2'),
+              Task('Aprender Flutter 3'),
+              Task('Aprender Flutter 4'),
+              Task('Aprender Flutter 5'),
+            ]
           ),
           floatingActionButton: FloatingActionButton(
             onPressed: () {},
           ),
         ));
+  }
+}
+
+class Task extends StatelessWidget {
+  final String name;
+  const Task(this.name, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        child: Stack(
+        children: [
+          Container(
+            color: Colors.blue,
+            height: 140,
+          ),
+          Container(
+            color: Colors.white,
+            height: 100,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  color: Colors.black26,
+                  width: 72,
+                  height: 100,
+                ),
+                SizedBox(
+                  width: 200,
+                  child: Text(
+                    name,
+                    style: const TextStyle(
+                      fontSize: 24,
+                      overflow: TextOverflow.ellipsis
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Icon(Icons.arrow_drop_up_outlined)
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+      )
+    );
   }
 }
