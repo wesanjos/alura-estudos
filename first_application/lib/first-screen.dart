1cb476f6e4d2fb408d0defd10a4ae9466782e426
Wesley Anjos <wanjos.andrade@gmail.com>
2023-09-15 20:29:56 -0300

    refactor: refatorando Difficulty

--- first_application/lib/first-screen.dart
+++ first_application/lib/first-screen.dart
@@ -1,77 +1,86 @@
-// Container(
-//           color: Colors.white,
-//           child: Column(
-//             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//             crossAxisAlignment: CrossAxisAlignment.center,
-//             children: [
-//               Stack(
-//                 alignment: AlignmentDirectional.center,
-//                 children: [
-//                   Container(
-//                     color: Colors.red,
-//                     width: 100,
-//                     height: 100,
-//                   ),
-//                   Container(
-//                     color: Colors.blue,
-//                     width: 50,
-//                     height: 50,
-//                   )
-//                 ],
-//               ),
-//               Stack(
-//                 alignment: AlignmentDirectional.center,
-//                 children: [
-//                   Container(
-//                     color: Colors.blue,
-//                     width: 100,
-//                     height: 100,
-//                   ),
-//                   Container(
-//                     color: Colors.red,
-//                     width: 50,
-//                     height: 50,
-//                   )
-//                 ],
-//               ),
-//               Row(
-//                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//                 crossAxisAlignment: CrossAxisAlignment.center,
-//                 children: [
-//                   Container(
-//                     color: Colors.cyan,
-//                     width: 50,
-//                     height: 50,
-//                   ),
-//                   Container(
-//                     color: Colors.purple,
-//                     width: 50,
-//                     height: 50,
-//                   ),
-//                   Container(
-//                     color: Colors.brown,
-//                     width: 50,
-//                     height: 50,
-//                   )
-//                 ],
-//               ),
-//               Container(
-//                 color: Colors.amber,
-//                 height: 30,
-//                 width: 300,
-//                 child: const Text(
-//                   'Hello world Flutter',
-//                   style: TextStyle(
-//                     color: Colors.black,
-//                     fontSize: 22,
-//                   ),
-//                   textAlign: TextAlign.center,
-//                 ),
-//               ),
-//               ElevatedButton(
-//                   onPressed: () {
-//                     print('Click Button');
-//                   },
-//                   child: const Text('Button'))
-//             ],
-//           )),
+import 'package:flutter/material.dart';
+
+class MyFirstWidget extends StatelessWidget {
+  const MyFirstWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Container(
+                  color: Colors.red,
+                  width: 100,
+                  height: 100,
+                ),
+                Container(
+                  color: Colors.blue,
+                  width: 50,
+                  height: 50,
+                )
+              ],
+            ),
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Container(
+                  color: Colors.blue,
+                  width: 100,
+                  height: 100,
+                ),
+                Container(
+                  color: Colors.red,
+                  width: 50,
+                  height: 50,
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.cyan,
+                  width: 50,
+                  height: 50,
+                ),
+                Container(
+                  color: Colors.purple,
+                  width: 50,
+                  height: 50,
+                ),
+                Container(
+                  color: Colors.brown,
+                  width: 50,
+                  height: 50,
+                )
+              ],
+            ),
+            Container(
+              color: Colors.amber,
+              height: 30,
+              width: 300,
+              child: const Text(
+                'Hello world Flutter',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 22,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  print('Click Button');
+                },
+                child: const Text('Button'))
+          ],
+        ));
+  }
+}
