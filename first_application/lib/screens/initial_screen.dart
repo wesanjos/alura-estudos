a067fdf575ad37cab45f326fa865468cbea4b61c
Wesley Anjos <wanjos.andrade@gmail.com>
2023-10-09 23:34:28 -0300

    feat: estruturação nível global

--- first_application/lib/screens/initial_screen.dart
+++ first_application/lib/screens/initial_screen.dart
@@ -17,9 +17,54 @@ class _InitialScreenState extends State<InitialScreen> {
       appBar: AppBar(
         title: const Text('Tarefas'),
       ),
-      body: ListView(
-          children: TaskInherited.of(context)!.taskList,
-          padding: EdgeInsets.only(top: 8, bottom: 70)),
+      body: Column(
+        children: [
+          Padding(
+            padding:
+                const EdgeInsets.only(top: 5, left: 16, right: 16, bottom: 24),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      'Nível global',
+                    ),
+                    TextButton(
+                      style: ButtonStyle(
+                        padding: MaterialStateProperty.all(
+                          EdgeInsets.zero,
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: const Column(
+                        children: [
+                          Icon(
+                            Icons.refresh,
+                            size: 18,
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+                const LinearProgressIndicator(
+                  value: 1 / 10,
+                  color: Colors.blueAccent,
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              padding: const EdgeInsets.only(top: 8, bottom: 70),
+              children: TaskInherited.of(context)!.taskList,
+            ),
+          ),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(
